14b9edbec0182910394ef71f5e80658e5e009152
Ștefan-Gabriel Muscalu <legraphista@users.noreply.github.com>
2019-08-19 15:50:07 +0300

    feat(filter:zoom): aspect ratio manipulation and schedule in zoom (#17)
    * Add: support from aspect ratio in vf_zoom
    * Update: use av_clip_c instead of FFMIN/MAX
    * add(filter:zoom): scheduling file and better stability
    * update(filter:zoom): better schedule input error logs
    * update(filter:zoom): better stability and more checks

--- libavfilter/vf_zoom.c
+++ libavfilter/vf_zoom.c
@@ -34,6 +34,8 @@
 #include "./internal.h"
 #include "./video.h"
 #include "../libswscale/swscale.h"
+#include <stdio.h>
+#include <string.h>
 
 static const char *const var_names[] = {
         "z", "zoom",/// -> last zoom
@@ -56,12 +58,19 @@ typedef struct ZoomContext {
     FFDrawContext             dc;
     const AVPixFmtDescriptor* desc;
 
+    char*           schedule_file_path;
+    double*         schedule;
+    unsigned long   schedule_size;
+    unsigned long   schedule_index;
 
+    double          zoom_max;
     double          zoom;
     double          x;
     double          y;
     int             interpolation;
     FFDrawColor     fillcolor;
+
+    double          outAspectRatio;
 
     char*           zoom_expr_str;
     AVExpr*         zoom_expr;
@@ -94,10 +103,12 @@ enum {
 #define OFFSET(x) offsetof(ZoomContext, x)
 #define FLAGS AV_OPT_FLAG_FILTERING_PARAM|AV_OPT_FLAG_VIDEO_PARAM
 static const AVOption zoom_options[] = {
+    { "schedule",           "binary file of <double> xyzxyzxyz...", OFFSET(schedule_file_path), AV_OPT_TYPE_STRING, {.str=""},       CHAR_MIN, CHAR_MAX, FLAGS },
     { "zoom",               "set zoom offset expression",           OFFSET(zoom_expr_str),  AV_OPT_TYPE_STRING, {.str="1"},          CHAR_MIN, CHAR_MAX, FLAGS },
     { "z",                  "set zoom offset expression",           OFFSET(zoom_expr_str),  AV_OPT_TYPE_STRING, {.str="1"},          CHAR_MIN, CHAR_MAX, FLAGS },
     { "x",                  "set x offset expression",              OFFSET(x_expr_str),     AV_OPT_TYPE_STRING, {.str="0.5"},        CHAR_MIN, CHAR_MAX, FLAGS },
     { "y",                  "set y offset expression",              OFFSET(y_expr_str),     AV_OPT_TYPE_STRING, {.str="0.5"},        CHAR_MIN, CHAR_MAX, FLAGS },
+    { "ar",                 "set aspect ratio",                     OFFSET(outAspectRatio), AV_OPT_TYPE_DOUBLE, {.dbl=0},          0.00    ,   100   , FLAGS },
     { "fillcolor",          "set color for background",             OFFSET(fillcolor.rgba), AV_OPT_TYPE_COLOR,  {.str="black@0"},  CHAR_MIN, CHAR_MAX, FLAGS },
 
     { "interpolation",      "enable interpolation when scaling",    OFFSET(interpolation),  AV_OPT_TYPE_INT,    {.i64=FAST_BILINEAR}, SWS_FAST_BILINEAR,   SPLINE, FLAGS, "interpolation"},
@@ -145,6 +156,68 @@ static int config_props(AVFilterLink *inlink)
     zoom->var_values[VAR_X] = 0.5;
     zoom->var_values[VAR_Y] = 0.5;
     zoom->var_values[VAR_T] = NAN;
+    zoom->schedule = NULL;
+    zoom->schedule_size = 0;
+    zoom->schedule_index = 0;
+
+    if(zoom->outAspectRatio == 0) {
+      zoom->outAspectRatio = 1.0 * inlink->w / inlink->h;
+    }
+
+    if (zoom->outAspectRatio <= 1) {
+      zoom->zoom_max = FFMIN(zoom->outAspectRatio * inlink->h, inlink->h);
+    }else{
+      zoom->zoom_max = FFMIN(inlink->w, inlink->w / zoom->outAspectRatio);
+    }
+
+    if (strlen(zoom->schedule_file_path) > 0) {
+      FILE *f = av_fopen_utf8(zoom->schedule_file_path, "r");
+      uint64_t file_size = 0;
+
+      if (!f) {
+        int ret = AVERROR(errno);
+        av_log(zoom, AV_LOG_ERROR, "Cannot open file '%s' for reading schedule: %s\n",
+               zoom->schedule_file_path, av_err2str(ret));
+        return ret;
+      }
+
+      fseek(f, 0L, SEEK_END);
+      file_size = ftell(f);
+      fseek(f, 0L, SEEK_SET);
+
+      if(file_size == 0){
+        av_log(zoom, AV_LOG_ERROR, "File '%s' contents are empty, file size is 0\n", zoom->schedule_file_path);
+        return AVERROR(EINVAL);
+      }
+
+      if(file_size % sizeof(double) != 0) {
+        av_log(zoom, AV_LOG_ERROR, "File '%s' contents are unaliged to double. File size %ld should be %ld\n",
+            zoom->schedule_file_path,
+            file_size,
+            file_size / sizeof(double) * sizeof(double));
+        return AVERROR(EINVAL);
+      }
+
+      unsigned long num_doubles = file_size / sizeof(double);
+
+      if(num_doubles % 3 != 0) {
+        av_log(zoom, AV_LOG_ERROR, "File '%s' double values are unaliged to XYZXYZXYZ... (not divisible by 3). Value count is %ld, should be %ld\n",
+            zoom->schedule_file_path,
+            num_doubles,
+            num_doubles / 3 * 3);
+        return AVERROR(EINVAL);
+      }
+
+      zoom->schedule_size = file_size / sizeof(double) / 3;
+
+      zoom->schedule = av_malloc(sizeof(char) * file_size);
+      if(zoom->schedule == NULL) {
+        return AVERROR(ENOMEM);
+      }
+      fread(zoom->schedule, sizeof(char), file_size, f);
+      fclose(f);
+    }
+
 
     int ret;
     if(
@@ -161,6 +234,15 @@ static int config_props(AVFilterLink *inlink)
 }
 static int config_output(AVFilterLink *outlink)
 {
+    ZoomContext *s = outlink->src->priv;
+
+    const double aspectRatio = s->outAspectRatio;
+
+    const int in_w = outlink->src->inputs[0]->w;
+    const int in_h = outlink->src->inputs[0]->h;
+
+    outlink->w = aspectRatio <= 1 ? aspectRatio * in_h : in_w;
+    outlink->h = aspectRatio <= 1 ? in_h : in_w / aspectRatio;
     return 0;
 }
 
@@ -197,15 +279,22 @@ static int zoom_out(ZoomContext *zoom, AVFrame *in, AVFrame *out, AVFilterLink *
     const int in_h  = in->height;
     const int in_f  = in->format;
 
-          int out_w = out->width * zoom_val;
-          int out_h = out->height * zoom_val;
+          int out_w = in->width * zoom_val;
+          int out_h = in->height * zoom_val;
     const int out_f = outlink->format;
+
+    int adjusted_in_w = in_w;
+    int adjusted_in_h = in_h;
+
+    const double aspectRatio = zoom->outAspectRatio;
+    if (aspectRatio <= 1) {
+      adjusted_in_w = aspectRatio * in_h;
+    }else{
+      adjusted_in_h = in_w / aspectRatio;
+    }
 
     const double x  = zoom->x;
     const double y  = zoom->y;
-
-    if(out_w % 2 == 1) out_w--;
-    if(out_h % 2 == 1) out_h--;
 
     if(out_h <= 0 || out_w <= 0)
         goto bypass;
@@ -231,8 +320,11 @@ static int zoom_out(ZoomContext *zoom, AVFrame *in, AVFrame *out, AVFilterLink *
     sws_freeContext(zoom->sws);
     zoom->sws = NULL;
 
-    const int dx = av_clip_c(in_w * x - out_w/2, 0, in_w - out_w);
-    const int dy = av_clip_c(in_h * y - out_h/2, 0, in_h - out_h);
+    const int dx = av_clip_c(adjusted_in_w * x - out_w/2, 0, adjusted_in_w - out_w);
+    const int dy = av_clip_c(adjusted_in_h * y - out_h/2, 0, adjusted_in_h - out_h);
+    av_log(zoom, AV_LOG_DEBUG, "dx: %d dy: %d\n", dx, dy);
+    av_log(zoom, AV_LOG_DEBUG, "in_w: %d in_h: %d\n", in_w, in_h);
+    av_log(zoom, AV_LOG_DEBUG, "adjusted_in_w: %d adjusted_in_h: %d\n", adjusted_in_w,adjusted_in_h);
 
     ff_copy_rectangle2(&zoom->dc,
                        out->data, out->linesize,
@@ -263,6 +355,13 @@ static int zoom_in (ZoomContext *zoom, AVFrame *in, AVFrame *out, AVFilterLink *
           int in_h  = in->height / zoom_val;
     const int in_f  = in->format;
 
+    const double aspectRatio = zoom->outAspectRatio;
+    if (aspectRatio <= 1) {
+      in_w = aspectRatio * in_h;
+    }else{
+      in_h = in_w / aspectRatio;
+    }
+
     const int out_w = out->width;
     const int out_h = out->height;
     const int out_f = outlink->format;
@@ -270,14 +369,16 @@ static int zoom_in (ZoomContext *zoom, AVFrame *in, AVFrame *out, AVFilterLink *
     const double x  = zoom->x;
     const double y  = zoom->y;
 
-    if(in_w % 2 == 1) in_w--;
-    if(in_h % 2 == 1) in_h--;
-
     if(out_h <= 0 || out_w <= 0)
         goto bypass;
 
-    const int dx = av_clip_c(out_w * x - in_w/2, 0, out_w - in_w);
-    const int dy = av_clip_c(out_h * y - in_h/2, 0, out_h - in_h);
+    av_log(zoom, AV_LOG_DEBUG, "in_w: %d in_h: %d\n", in_w, in_h);
+    av_log(zoom, AV_LOG_DEBUG, "out_w: %d out_h: %d\n", out_w, out_h);
+
+    const int dx = av_clip_c(in->width * x - in_w / 2, 0, in->width - in_w);
+    const int dy = av_clip_c(in->height * y - in_h / 2, 0, in->height - in_h);
+    av_log(zoom, AV_LOG_DEBUG, "x: %0.3f y: %0.3f\n", x, y);
+    av_log(zoom, AV_LOG_DEBUG, "dx: %d dy: %d\n", dx, dy);
 
 
     int px[4], py[4];
@@ -335,25 +436,52 @@ static int filter_frame(AVFilterLink *inlink, AVFrame *in)
     const int in_w = in->width;
     const int in_h = in->height;
 
-    out = ff_get_video_buffer(outlink, outlink->w, outlink->h);
+    const int out_w = outlink->w;
+    const int out_h = outlink->h;
+
+    out = ff_get_video_buffer(outlink, out_w, out_h);
     if (!out) {
         av_frame_free(&in);
         return AVERROR(ENOMEM);
     }
     av_frame_copy_props(out, in);
 
-
     // eval T (time)
     zoom->var_values[VAR_T] = in->pts == AV_NOPTS_VALUE ?
-                                NAN :
-                                in->pts * av_q2d(inlink->time_base);
-    // eval Z (zoom)
-    zoom_val = zoom->zoom = zoom->var_values[VAR_Z] = zoom->var_values[VAR_ZOOM] = av_expr_eval(zoom->zoom_expr, zoom->var_values, NULL);
+                            NAN :
+                            in->pts * av_q2d(inlink->time_base);
 
-    // eval x/y
-    zoom->x = zoom->var_values[VAR_X] = av_expr_eval(zoom->x_expr, zoom->var_values, NULL);
-    zoom->y = zoom->var_values[VAR_Y] = av_expr_eval(zoom->y_expr, zoom->var_values, NULL);
+    if(zoom->schedule){
+      unsigned long offset = zoom->schedule_index * 3;
+      zoom->schedule_index += 1;
 
+      double *schedule = zoom->schedule;
+
+      if(offset >= zoom->schedule_size * 3){
+        av_log(zoom, AV_LOG_WARNING, "schedule index %ld exceeds schedule size of %ld (%ld values)\n", offset / 3, zoom->schedule_size, zoom->schedule_size * 3);
+        offset = zoom->schedule_size * 3 - 3;
+      }
+
+      av_log(zoom, AV_LOG_DEBUG, "schedule index %ld x:%.3f y:%.3f z:%.3f\n", offset / 3, zoom->schedule_size, zoom->schedule_size * 3);
+
+      // XYZ
+      zoom->x = zoom->var_values[VAR_X] = schedule[offset + 0];
+      zoom->y = zoom->var_values[VAR_Y] = schedule[offset + 1];
+      zoom_val = zoom->zoom = zoom->var_values[VAR_Z] = zoom->var_values[VAR_ZOOM] = schedule[offset + 2];
+    }else{
+      // eval Z (zoom)
+      zoom_val = zoom->zoom = zoom->var_values[VAR_Z] = zoom->var_values[VAR_ZOOM] = av_expr_eval(zoom->zoom_expr,
+                                                                                                  zoom->var_values,
+                                                                                                  NULL);
+      // eval x/y
+      zoom->x = zoom->var_values[VAR_X] = av_expr_eval(zoom->x_expr, zoom->var_values, NULL);
+      zoom->y = zoom->var_values[VAR_Y] = av_expr_eval(zoom->y_expr, zoom->var_values, NULL);
+    }
+
+    if(zoom->zoom < 0 || zoom->zoom >= zoom->zoom_max) {
+      av_log(zoom, AV_LOG_WARNING, "zoom value %.2f is out of range of [0-%.3f]\n", zoom->zoom, zoom->zoom_max);
+      zoom->zoom = av_clipd_c(zoom->zoom, 0, zoom->zoom_max);
+    }
     if(zoom->x < 0 || zoom->x > 1){
         av_log(zoom, AV_LOG_WARNING, "x position %.2f is out of range of [0-1]\n", zoom->x);
         zoom->x = av_clipd_c(zoom->x, 0, 1);
@@ -366,7 +494,7 @@ static int filter_frame(AVFilterLink *inlink, AVFrame *in)
     ff_fill_rectangle(&zoom->dc, &zoom->fillcolor,
                       out->data, out->linesize,
                       0, 0,
-                      in_w, in_h);
+                      out_w, out_h);
 
     // scale
     if(zoom_val == 1) {
@@ -375,8 +503,10 @@ static int filter_frame(AVFilterLink *inlink, AVFrame *in)
         ff_copy_rectangle2(&zoom->dc,
                            out->data, out->linesize,
                            in->data, in->linesize,
-                           0, 0, 0, 0,
-                           in_w, in_h);
+                           0, 0,
+                           av_clip_c(in_w * zoom->x - out_w / 2.0, 0, in_w - out_w),
+                           av_clip_c(in_h * zoom->y - out_h / 2.0, 0, in_h - out_h),
+                           out_w, out_h);
     } else if (zoom_val <= 0) {
         // if it's 0 or lower do nothing
         // noop
@@ -408,6 +538,8 @@ static av_cold void uninit(AVFilterContext *ctx)
     av_expr_free(zoom->y_expr);
 
     zoom->zoom_expr = NULL;
+    if (zoom->schedule != NULL)
+      av_freep(zoom->schedule);
 }
 
 static const AVFilterPad zoom_inputs[] = {
