b085ca5c222d9472e2f15e9a160bf33fbfe11a4a
Stefan-Gabriel Muscalu <stefan.gabriel.muscalu@gmail.com>
2020-10-15 18:57:46 +0300

    update(vf_zoom_opencl): support exact width/height output

--- libavfilter/vf_zoom_opencl.c
+++ libavfilter/vf_zoom_opencl.c
@@ -76,6 +76,7 @@ typedef struct ZoomOpenCLContext {
 
     int             desiredWidth;
     int             desiredHeight;
+    int             exact;
     double          outAspectRatio;
 
     char*           zoom_expr_str;
@@ -119,6 +120,7 @@ static const AVOption zoom_opencl_options[] = {
     { "ar",                 "set aspect ratio",                     OFFSET(outAspectRatio), AV_OPT_TYPE_DOUBLE, {.dbl=0},            0.00    ,   100   , FLAGS },
     { "width",              "set desired width",                    OFFSET(desiredWidth),   AV_OPT_TYPE_INT   , {.i64=-1},           -1      ,   65536 , FLAGS },
     { "height",             "set desired height",                   OFFSET(desiredHeight),  AV_OPT_TYPE_INT   , {.i64=-1},           -1      ,   65536 , FLAGS },
+    { "exact",              "set frame size is exact or div by 2",  OFFSET(exact),          AV_OPT_TYPE_BOOL  , {.i64=0},            0       ,   1     , FLAGS },
     { "fillcolor",          "set color for background",             OFFSET(fillcolor.rgba), AV_OPT_TYPE_COLOR,  {.str="black@0"},  CHAR_MIN, CHAR_MAX, FLAGS },
 
     { NULL }
@@ -245,11 +247,13 @@ static int config_output(AVFilterLink *outlink)
         outlink->h = s->desiredHeight;
     }
 
-    if(outlink->w % 2 != 0){
-      outlink->w -= 1;
-    }
-    if(outlink->h % 2 != 0){
-      outlink->h -= 1;
+    if(!s->exact) {
+        if (outlink->w % 2 != 0) {
+            outlink->w -= 1;
+        }
+        if (outlink->h % 2 != 0) {
+            outlink->h -= 1;
+        }
     }
     if(outlink->w <= 0){
       outlink->w = 2;
